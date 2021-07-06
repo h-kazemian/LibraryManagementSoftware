6757191a7c8b72f4206e141a58222e11b8d6d5f2
hosna kazemian <hkazemian2000@gmail.com>
2021-07-06 22:46:55 +0430

    Login and Register page design

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Request(object sender, RoutedEventArgs e)
+        {
+            MembershipPage membership = new MembershipPage();
+            membership.Show();
+            this.Close();
+        }
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordTxtBox.Text = PasswordBox.Password;
+            PasswordBox.Visibility = Visibility.Collapsed;
+            passwordTxtBox.Visibility = Visibility.Visible;
+        }
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = passwordTxtBox.Text;
+            passwordTxtBox.Visibility = Visibility.Collapsed;
+            PasswordBox.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MembershipPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+
+namespace WpfApp1
+{
+    public partial class MembershipPage : Window
+    {
+        public MembershipPage()
+        {
+            InitializeComponent();
+        }
+        private void LoadPicture_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                Photo.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordTxtBox.Text = PasswordBox.Password;
+            PasswordBox.Visibility = Visibility.Collapsed;
+            passwordTxtBox.Visibility = Visibility.Visible;
+        }
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = passwordTxtBox.Text;
+            passwordTxtBox.Visibility = Visibility.Collapsed;
+            PasswordBox.Visibility = Visibility.Visible;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
